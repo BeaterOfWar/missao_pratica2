db5601e2a2d0ad9cd85ebe094c95304086fc4bd9
Moisés Eduardo <moises.eduardogc@gmail.com>
2023-10-24 10:49:28 -0300

    receitas task 2

--- javascript/receitas.js
+++ javascript/receitas.js
@@ -36,3 +36,27 @@ let Receitas = [
       "no fogo baixo, envolva os morangos e passe no granulado.",
   },
 ];
+
+
+const  getListaIngredientes = (Receitas) => Receitas.ingredientes
+  .map(ingredientes => `<li>${ingredientes}</li>`)
+  .reduce((gerador, item) => gerador + item, '<ul class="ingredientes">') + '</ul>';
+
+
+const  getCard = (Receitas) => 
+  `<div class="card rounded-5 my-4" style="width: 300px; height: 530px;"'> 
+     <img src='${Receitas.imagem}' class="card-img-top">
+     <div class='card-body'>
+       <h2 class="card-title fs-4 p-2 fw-bold">${Receitas.nome}</h2>
+       <div class="mb-5 ingredientes">${getListaIngredientes(Receitas)}</div>
+       <hr>
+       <p class="card-text p-2">${Receitas.receita}</p>
+     </div>
+   </div>`;
+
+
+const  preencheCatalogo= () => 
+  document.getElementById(pnlCatalogo).innerHTML = Receitas
+    .map(Receitas => getCard(Receitas))
+    .reduce((gerador, item)=> gerador + item, '');
+
